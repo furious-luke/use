0743ee37a99d6f68c30035b9f4dafc02073e6a5e
Luke Hodkinson <furious.luke@gmail.coim>
2013-08-14 18:03:50 +1000

    Adding a placeholder for a resolver on the cxx_compiler class.

--- use/packages/cxx_compiler.py
+++ use/packages/cxx_compiler.py
@@ -1,6 +1,13 @@
 import use
+from ..Platform import platform
 
 class cxx_compiler(use.Package):
     name = 'C++ compiler'
     option_name = 'cxx'
     sub_packages = ['gcc', 'clangxx']
+
+    def resolve(self):
+
+        # If we're building on Darwin, favor clang.
+        if platform.os_name == 'darwin':
+            pass
