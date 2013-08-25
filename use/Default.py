3cb203da942ce7416b5b3cb16438a6c6724cd15c
Luke Hodkinson <furious.luke@gmail.coim>
2013-08-25 12:31:38 +1000

    A little cleaning up.

--- use/Default.py
+++
@@ -1,2 +0,0 @@
-def default(*args, **kwargs):
-    pass
--- use/tools/file.py
+++
@@ -1,9 +0,0 @@
-from Tool import Tool
-
-class File(Tool):
-
-    SHARED_LIBRARY=1
-    STATIC_LIBRARY=2
-
-    def identify(path):
-        pass
