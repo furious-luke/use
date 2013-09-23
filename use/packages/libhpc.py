0a217c60ca46ca16bc24044da51a3dae63503027
Luke Hodkinson <furious.luke@gmail.com>
2013-09-23 22:00:12 +1000

    Adding a download URL to libhpc.

--- use/packages/libhpc.py
+++ use/packages/libhpc.py
@@ -4,7 +4,14 @@ class Default(use.Version):
     version = 'default'
     headers = ['libhpc/libhpc.hh']
     libraries = ['hpc']
+    url = 'http://github.com/furious-luke/libhpc/archive/use.zip'
 
 class libhpc(use.Package):
     versions = [Default]
     dependencies = ['boost', 'mpi', 'hdf5', 'pugixml'] #, '?glut']
+
+    def build_handler(self):
+        config = 'use --enable-debug --enable-openmp --prefix {prefix}'
+        return [config,
+                'use',
+                'use install']
