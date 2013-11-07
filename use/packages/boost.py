df198724f1cf23e359068ddad25ab50653267275
Luke Hodkinson <furious.luke@gmail.com>
2013-11-07 13:21:00 +1100

    Making coroutine optional.

--- use/packages/boost.py
+++ use/packages/boost.py
@@ -10,12 +10,16 @@ from ..Platform import platform
 # class filesystem(use.Feature):
 #     libraries = ['boost_filesystem']
 
+class coroutines(use.Feature):
+    libraries = [['boost_coroutine'], ['boost_coroutine-mt']]
+
 class Default(use.Version):
     version = 'default'
     headers = ['boost/optional.hpp']
-    libraries = [['boost_system', 'boost_regex', 'boost_iostreams', 'boost_filesystem', 'boost_coroutine'],
-                 ['boost_system-mt', 'boost_regex-mt', 'boost_iostreams-mt', 'boost_filesystem-mt', 'boost_coroutine-mt']]
+    libraries = [['boost_system', 'boost_regex', 'boost_iostreams', 'boost_filesystem'],
+                 ['boost_system-mt', 'boost_regex-mt', 'boost_iostreams-mt', 'boost_filesystem-mt']]
     # features = [regex, iostreams, filesystem]
+    features = [coroutines]
 
 class boost(use.Package):
     default_builder = use.Builder
