995cf80d1e1466979241053e1ee7381ee2495f31
Luke Hodkinson <furious.luke@gmail.com>
2013-12-09 10:48:18 +1100

    Adding program options to boost.

--- use/packages/boost.py
+++ use/packages/boost.py
@@ -14,13 +14,17 @@ class coroutines(use.Feature):
     libraries = [['boost_coroutine', 'boost_context'],
                  ['boost_coroutine-mt', 'boost_context-mt']]
 
+class program_options(use.Feature):
+    libraries = [['boost_program_options',],
+                 ['boost_program_options-mt']]
+
 class Default(use.Version):
     version = 'default'
     headers = ['boost/optional.hpp']
     libraries = [['boost_system', 'boost_regex', 'boost_iostreams', 'boost_filesystem'],
                  ['boost_system-mt', 'boost_regex-mt', 'boost_iostreams-mt', 'boost_filesystem-mt']]
     # features = [regex, iostreams, filesystem]
-    features = [coroutines]
+    features = [coroutines, program_options]
 
 class boost(use.Package):
     default_builder = use.Builder
