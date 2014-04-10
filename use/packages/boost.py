11e885b3c03ca0ddd70a4772c3db63ab330b0154
Luke Hodkinson <furious.luke@gmail.com>
2014-04-10 18:38:11 +1000

    Adding chrono as a feature of boost.

--- use/packages/boost.py
+++ use/packages/boost.py
@@ -10,6 +10,10 @@ from ..Platform import platform
 # class filesystem(use.Feature):
 #     libraries = ['boost_filesystem']
 
+class chrono(use.Feature):
+    libraries = [['boost_chrono'],
+                 ['boost_chrono-mt']]
+
 class coroutines(use.Feature):
     libraries = [['boost_coroutine', 'boost_context'],
                  ['boost_coroutine-mt', 'boost_context-mt']]
@@ -19,12 +23,11 @@ class program_options(use.Feature):
                  ['boost_program_options-mt']]
 
 class Default(use.Version):
-    version = 'default'
     headers = ['boost/optional.hpp']
     libraries = [['boost_system', 'boost_regex', 'boost_iostreams', 'boost_filesystem'],
                  ['boost_system-mt', 'boost_regex-mt', 'boost_iostreams-mt', 'boost_filesystem-mt']]
     # features = [regex, iostreams, filesystem]
-    features = [coroutines, program_options]
+    features = [coroutines, program_options, chrono]
 
 class boost(use.Package):
     default_builder = use.Builder
