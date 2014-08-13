c9d60c727b5fba4ac2e2b110942252b55d40978f
Luke Hodkinson <furious.luke@gmail.coim>
2014-08-13 19:00:15 +1000

    Mild update to example script.

--- examples/usescript.py
+++ examples/usescript.py
@@ -2,3 +2,5 @@ argument('--prefix', default='/usr/local', help='installation path')
 argument('--enable-debug', help='debugging mode')
 argument('--enable-logging', help='logging switch')
 argument('--with-opengl', help='use opengl')
+
+gcc = use('gcc')
